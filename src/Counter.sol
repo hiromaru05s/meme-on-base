bd27825d4b70107e63d377b42773daa0750ad405
hiromaru05s <maruru836818@gmail.com>
2024-06-19 06:16:18 -0400

    setup base contract and foundry

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// src/PepeCoin.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract Counter {
-    uint256 public number;
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
+contract PepeCoin is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("PepeCoin", "PEPE") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
     }
 }
