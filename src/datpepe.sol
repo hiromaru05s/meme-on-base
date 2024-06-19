e807b194bd0a8320b99814d141e94e44082e804c
hiromaru05s <maruru836818@gmail.com>
2024-06-19 09:10:41 -0400

    deleted burn function and update function, also made some adjustments on src structure

--- src/datpepe.sol
+++ src/datpepe.sol
@@ -6,7 +6,6 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract PepeCoin is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("PepeCoin", "PEPE") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("Dat Pepe", "DPP", 10000000000 * 10**18) Ownable(msg.sender) {
     }
 }
