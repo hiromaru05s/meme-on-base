6642e4b3bb52e02c485a9af522dfa2cae41b8f05
hiromaru05s <maruru836818@gmail.com>
2024-06-19 09:18:41 -0400

    added deployment script

--- src/datpepe.sol
+++ src/datpepe.sol
@@ -1,11 +1,10 @@
-// src/PepeCoin.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract PepeCoin is ERC20, Ownable {
+contract DatPepe is ERC20, Ownable {
     constructor() ERC20("Dat Pepe", "DPP", 10000000000 * 10**18) Ownable(msg.sender) {
     }
 }
